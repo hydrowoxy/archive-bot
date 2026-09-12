b328711da180db1b7eb166ce13d0b81b366cda6c
Salma Baig <76014459+hydrowoxy@users.noreply.github.com>
2026-09-12 12:19:54 -0400

    more setup

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 who knows...
+
+# Setup
+
+1. Create a config.json file with the following format
+{
+    "clientId" : "",
+    "guildId" : "",
+    "token" : "",
+    "wikiApiKey": "",
+    "githubToken": ""
+}
+
+Where:
+- The clientId is the ID of your bot,
+- guildId is of your server,
+- the token is the bot token,
+- the wikiApiKey is the api key from the admin panel of the Wiki, and
+- the githubToken is a (classic) personal access token for a GitHub account with access to the Baja Racing org
+
+2. Then, run npm i in order to install all node modules
+3. Run deploy-commands.js to send the commands to the discord server.
+4. Run npm start to begin hosting the bot
+5. Host index.js as long as you'd like to run the discord bot
+
+# Docker
+- tbd
---
+++ index.js
@@ -0,0 +1,46 @@
+// Require the necessary default, discordJS and mongo classes
+const fs = require('fs');
+const { Client, Collection, GatewayIntentBits, Partials } = require('discord.js');
+const { token } = require('./config.json');
+
+// Create a new client instance
+const client = new Client({ 
+	intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.MessageContent,
+		//GatewayIntentBits.GuildMessageReactions,
+	], 
+	partials: [
+		Partials.Message, 
+		Partials.Channel,
+		//Partials.Reaction
+	]
+});
+
+// Create commands list
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+// Loop through the command files and add them to the commands list
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	// Set a new item in the Collection
+	// With the key as the command name and the value as the exported module
+	client.commands.set(command.data.name, command);
+}
+
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+
+// Loop through the event files and add them to the events list
+for (const file of eventFiles) {
+	const event = require(`./events/${file}`);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
+
+// Login to Discord with your client's token
+client.login(token);
