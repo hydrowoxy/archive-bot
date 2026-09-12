5ff7052073cbfa3913eaca8aa7d7db1e069ee615
Salma Baig <76014459+hydrowoxy@users.noreply.github.com>
2026-09-12 12:31:14 -0400

    whoops

--- deploy-commands.js
+++ deploy-commands.js
@@ -12,6 +12,7 @@ for (const file of commandFiles) {
 	commands.push(command.data.toJSON());
 }
 
+//const rest = new REST({ version: '9' }).setToken(token);
 const rest = new REST({ version: '9' }).setToken(token);
 
 rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
