1b0680a4311c606fddc69e62a6a272010a4c4533
Salma Baig <76014459+hydrowoxy@users.noreply.github.com>
2026-09-12 12:30:53 -0400

    change dependencies

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
-const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord-api-types/v9');
+//const { REST } = require('@discordjs/rest');
+//const { Routes } = require('discord-api-types/v9');
+const { REST, Routes } = require('discord.js');
 const { clientId, guildId, token } = require('./config.json');
 
 const commands = [];
